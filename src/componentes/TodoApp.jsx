5f6919919c9a65b3ac15c358e895263bbc004cde
erocksom47@gmail.com <eroksom47@gmail.com>
2026-07-10 01:26:36 -0600

    hooks

--- src/componentes/TodoApp.jsx
+++ src/componentes/TodoApp.jsx
@@ -4,13 +4,21 @@ function TodoApp({notasApp}){//el nombre de ser igual al del atributo que se pas
     // const { notas} = props
 
     //console.log(notasApp);
+
+
+    const mostrarNotas = (event) =>{
+        console.log(event.target);
+    };
+
+    console.log(notasApp);
     return (
         <>
             <h1>App de Notas </h1>
-            <h2>{2+2}</h2>
             <ul>
                 <li>{notasApp[0].text}</li>
                 <li>{notasApp[1].text}</li>
+                <button onClick={mostrarNotas}>
+                    Mostrar notas por consola</button>
             </ul>
         </>
     );
